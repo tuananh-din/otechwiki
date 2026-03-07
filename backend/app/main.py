6faff8ba4145cbed6948c4d174ff9316a6538d78
Đinh Tuấn Anh <tuananh@MacBook-Air-cua-Tuan-Anh.local>
2026-03-07 20:24:04 +0700

    CORS allow all origins, frontend API URL set to cloudflare tunnel

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.1.23:3001", "http://192.168.1.23:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
